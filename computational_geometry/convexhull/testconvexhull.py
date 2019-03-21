ef6db6fe4af8bc333f1b8b67fdb7264be7027a70
romanstrazanec <roman.strazanec007@gmail.com>
2019-03-21 19:23:54 +0100

    fix indentation

--- computational_geometry/convexhull/testconvexhull.py
+++ computational_geometry/convexhull/testconvexhull.py
@@ -5,23 +5,23 @@ from point import Point
 
 
 def showpoints(points, **kwargs):
-  return plt.scatter([p.x for p in points], [p.y for p in points], **kwargs)
+    return plt.scatter([p.x for p in points], [p.y for p in points], **kwargs)
 
 
 def showline(points, **kwargs):
-  return plt.plot([p.x for p in points], [p.y for p in points], **kwargs)
+    return plt.plot([p.x for p in points], [p.y for p in points], **kwargs)
 
 
 def printpoints(points):
-  for i, p in enumerate(points):
-    print(p, end="\n" if i % 4 == 3 else ", ")
+    for i, p in enumerate(points):
+        print(p, end="\n" if i % 4 == 3 else ", ")
 
 
-points = [Point(3,0), Point(8,0), Point(5,1), Point(1,2),
-  Point(0,3), Point(4,3), Point(8,4), Point(10,4),
-  Point(2,5), Point(6,5), Point(4,6), Point(9,7),
-  Point(1,8), Point(5,8), Point(7,9), Point(3,10),
-]
+points = [Point(3, 0), Point(8, 0), Point(5, 1), Point(1, 2),
+          Point(0, 3), Point(4, 3), Point(8, 4), Point(10, 4),
+          Point(2, 5), Point(6, 5), Point(4, 6), Point(9, 7),
+          Point(1, 8), Point(5, 8), Point(7, 9), Point(3, 10),
+          ]
 
 ch = convexhull(points)
 showpoints(points)
@@ -31,7 +31,7 @@ plt.clf()
 
 points.clear()
 for i in range(20):
-  points.append(Point(random()*10 - 5, random()*10 - 5))
+    points.append(Point(random()*10 - 5, random()*10 - 5))
 
 ch = convexhull(points)
 showpoints(points)
