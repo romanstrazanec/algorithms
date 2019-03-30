b9133d200b2cc306f5fea872be0859f11faef986
romanstrazanec <roman.strazanec007@gmail.com>
2019-03-30 17:08:58 +0100

    effective fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,20 @@
-def fib(i):
+def fib(i: int) -> int:
     if 0 < i < 3:
         return 1
     if i > 2:
         return fib(i-1) + fib(i-2)
     return 0
+
+
+def difffib(i: int, d: int = 0) -> int:
+    return fib(1+d)*fib(i-2-d) + fib(2+d)*fib(i-1-d)
+
+
+def effib(i: int) -> int:
+    if i == 1:
+        return 1
+    if i < 1:
+        return 0
+    hi = i // 2
+    f = effib(hi)
+    return f*f + effib(hi+1)**2 if i % 2 else f*f + 2*f*effib(hi-1)
