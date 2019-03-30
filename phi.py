0f48ef314e10fd8c4f75b8bdcf62bb9f5cbc0a78
romanstrazanec <roman.strazanec007@gmail.com>
2019-03-30 12:59:21 +0100

    phi using fibonacci sequence and golden_ratio formula

--- phi.py
+++ phi.py
@@ -1,5 +1,18 @@
+from fibonacci import fib
+
+
 def phi():
     try:
         return 1 + 1 / phi()
     except RecursionError:
         return 1
+
+
+def fi(i=27):
+    if i < 3:
+        return 0
+    return fib(i) / fib(i-1)
+
+
+def golden_ratio():
+    return (1 + 5**(.5))*.5
