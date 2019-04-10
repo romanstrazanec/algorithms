b55e7232fc853b246695a4b9d71458df5ea4594d
romanstrazanec <roman.strazanec007@gmail.com>
2019-04-10 22:21:14 +0200

    from data_structures import *

--- data_structures/__init__.py
+++ data_structures/__init__.py
@@ -1,2 +1,15 @@
-__all__ = ['avltree', 'binaryheap', 'binarysearchtree', 'binarytree', 'deque',
-           'node', 'orderedlist', 'queue', 'stack', 'treenode', 'unorderedlist']
+from avltree import AVLTree
+from binaryheap import BinaryHeap
+from binarysearchtree import BinarySearchTree
+from binarytree import BinaryTree
+from deque import Deque
+from node import Node
+from orderedlist import OrderedList
+from queue import Queue
+from stack import Stack
+from treenode import TreeNode
+from unorderedlist import List
+
+
+__all__ = [AVLTree, BinaryHeap, BinarySearchTree, BinaryTree, Deque,
+           Node, OrderedList, Queue, Stack, TreeNode, List]
